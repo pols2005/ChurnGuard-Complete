c1c8bb9c7783e6c7b308fc8619b92508640a7f4c
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-21 12:55:16 -0800

    implement speedometer component to show average churn probability

---
+++ churn-prediction/src/GaugeMeter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactSpeedometer from "react-d3-speedometer";
+
+const GaugeMeter = ({churnProbability}) => {
+
+let churnValue = Math.round(churnProbability * 100 * 100)/ 100;
+
+return(
+<div >
+  <div className="text-center font-semibold p-3">Churn Probability</div>
+  <div className="flex justify-center">
+  <ReactSpeedometer
+  value={churnValue}
+  minValue={0}
+  maxValue={100}
+  endColor={"#FF0000"}
+  startColor={"#33CC33"}
+  needleColor="steelblue"
+  needleTransitionDuration={4000}
+  needleTransition="easeElastic"/>
+</div>
+</div>
+)
+};
+
+export default GaugeMeter;
