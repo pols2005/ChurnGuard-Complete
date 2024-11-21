c34ace2c294b8b48991e3e79a62921331e5b4ba6
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-20 23:14:16 -0800

    set up api/customers and api/customer/customerid endponts and extract corresponding data from csv file containing dataset from Kaggle

--- churn-prediction/server/app.py
+++ churn-prediction/server/app.py
@@ -5,10 +5,19 @@ import pandas as pd
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
+@app.route('/api/customers', methods=['GET'])
+def get_customers():
     df = pd.read_csv('churn.csv')
-    return df.to_json(orient='records')
+    customers = df[['CustomerId', 'Surname']].to_dict(orient='records')
+    return jsonify(customers)
+
+@app.route('/api/customer/<int:customer_id>', methods=['GET'])
+def get_customer_details(customer_id):
+    df = pd.read_csv('churn.csv')
+    customer = df[df['CustomerId'] == customer_id].to_dict(orient='records')
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+    return jsonify({'customer': customer[0]})
 
 
 if __name__ == '__main__':
