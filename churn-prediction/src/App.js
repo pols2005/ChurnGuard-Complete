c016bb74cb36737d4d133676c34ae3134d96b3be
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-21 02:21:04 -0800

    add button to show probabilities and define state and fetch functions to fetch average probability of churn and probabilities for various models

--- churn-prediction/src/App.js
+++ churn-prediction/src/App.js
@@ -7,6 +7,8 @@ const App = () => {
   const [customers, setCustomers] = useState([]);
   const [selectedCustomer, setSelectedCustomer] = useState(null);
   const [customerDetails, setCustomerDetails] = useState({});
+  const [churnProbability, setChurnProbability] = useState(null);
+  const [modelProbabilities, setModelProbabilities] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:5001/api/customers')
@@ -31,10 +33,45 @@ const App = () => {
       });
   };
 
+  const handleShowChurnProbability = () => {
+    if (!selectedCustomer) {
+      alert('Please select a customer first!');
+      return;
+    }
+
+    // Fetch average churn probability
+    fetch(`http://localhost:5001/api/customer/${selectedCustomer.value}/churn-probability`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.error) {
+          setChurnProbability(data.average_probability);
+          console.log("avg", data);
+        }
+      });
+
+    // Fetch probabilities from individual models
+    fetch(`http://localhost:5001/api/customer/${selectedCustomer.value}/churn-model-probabilities`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.error) {
+          setModelProbabilities(data.model_probabilities);
+          console.log("models", data);
+        }
+      });
+  }
+
   return (
     <div className="bg-gray-900 text-gray-100 min-h-screen flex flex-col items-center py-10">
       <Heading />
       <CustomerForm customers={customers} selectedCustomer={selectedCustomer} customerDetails={customerDetails} handleCustomerSelect={handleCustomerSelect}/>
+      <div className="mt-6">
+        <button
+          onClick={handleShowChurnProbability}
+          className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          Show Customer Churn Probability
+        </button>
+      </div>
     </div>
   );
 };
