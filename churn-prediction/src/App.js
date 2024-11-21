7951983b401010f848d4515079ef817143d12841
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-20 23:11:50 -0800

    App component to set up customers and selected customers state and fetch corresponding data from backend

--- churn-prediction/src/App.js
+++ churn-prediction/src/App.js
@@ -1,23 +1,42 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Heading from './Heading';
+import CustomerForm from './CustomerForm';
 
-function App() {
+const App = () => {
+
+  const [customers, setCustomers] = useState([]);
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [customerDetails, setCustomerDetails] = useState({});
+
   useEffect(() => {
-    fetch('http://localhost:5001/api/data')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => console.log(data))
-      .catch((error) => console.error('Fetch error:', error));
+    fetch('http://localhost:5001/api/customers')
+      .then((response) => response.json())
+      .then((data) => {
+        const formattedCustomers = data.map((customer) => ({
+          value: customer.CustomerId,
+          label: `${customer.Surname} (${customer.CustomerId})`,
+        }));
+        setCustomers(formattedCustomers);
+      });
   }, []);
 
+  const handleCustomerSelect = (selectedOption) => {
+    setSelectedCustomer(selectedOption);
+    fetch(`http://localhost:5001/api/customer/${selectedOption.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.error) {
+          setCustomerDetails(data.customer);
+        }
+      });
+  };
+
   return (
-    <div className="App">
-      <h1>Check the console for data</h1>
+    <div className="bg-gray-900 text-gray-100 min-h-screen flex flex-col items-center py-10">
+      <Heading />
+      <CustomerForm customers={customers} selectedCustomer={selectedCustomer} customerDetails={customerDetails} handleCustomerSelect={handleCustomerSelect}/>
     </div>
   );
-}
+};
 
 export default App;
