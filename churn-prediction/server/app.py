af1490eb076e9583e5b0415c95643651953ea155
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-20 21:13:32 -0800

    dataset from Kaggle

--- churn-prediction/server/app.py
+++ churn-prediction/server/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-  return jsonify({'message': 'Hello'})
+    df = pd.read_csv('churn.csv')
+    return df.to_json(orient='records')
+
 
 if __name__ == '__main__':
   app.run(debug = True, port=5001)
-
-
