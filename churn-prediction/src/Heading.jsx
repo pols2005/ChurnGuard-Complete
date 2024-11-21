7d850861a1a73dff36c353155203897fd2bfceb6
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-20 23:12:59 -0800

    add heading component

---
+++ churn-prediction/src/Heading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Heading = () => {
+  return (
+    <header className="mb-6 text-center">
+      <h1 className="text-4xl font-bold">ChurnGuard</h1>
+      <p className="text-gray-400 mt-2">
+        An effective solution for customer churn prediction and mitigation
+      </p>
+    </header>
+  );
+};
+
+export default Heading;
+
