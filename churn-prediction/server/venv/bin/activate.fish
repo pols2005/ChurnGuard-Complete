c18c4baef1a4ebd341ad3a10bf4856e2292c7ad0
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-20 17:39:12 -0800

    reinstall venv and python dependencies

--- churn-prediction/server/venv/bin/activate.fish
+++ churn-prediction/server/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/pallavikishore/Desktop/Projects/Churn-Prediction-with-ML/churn-prediction/venv"
+set -gx VIRTUAL_ENV "/Users/pallavikishore/Desktop/Projects/Churn-Prediction-with-ML/churn-prediction/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
