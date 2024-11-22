36b5282f5abf36044563900a5492f3ab51ae16e5
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-21 23:59:12 -0800

    Add Explanation component

---
+++ churn-prediction/src/Explanation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Explanation = ({explanation}) => {
+
+return (
+  <div className="p-8 rounded-lg max-w-6xl w-full mx-auto"
+  style={{
+    background: `linear-gradient(135deg, #01497c, #014f86, #2a6f97, #2c7da0, #468faf)`,
+  }}>
+    <div className="text-2xl font-semibold mb-3">Explanation of Prediction:</div>
+    <div>{explanation}</div>
+  </div>
+)
+};
+
+export default Explanation;
