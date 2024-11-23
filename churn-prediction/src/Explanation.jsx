e93953ccdabdce0cef3a11f191bdda9aa56465b7
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-11-22 16:23:15 -0800

    styling changes to explanation component

--- churn-prediction/src/Explanation.jsx
+++ churn-prediction/src/Explanation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Explanation = ({explanation}) => {
 
 return (
-  <div className="p-8 rounded-lg max-w-6xl w-full mx-auto"
+  <div className="p-8 rounded-lg max-w-6xl w-full mx-auto mt-3"
   style={{
     background: `linear-gradient(135deg, #01497c, #014f86, #2a6f97, #2c7da0, #468faf)`,
   }}>
